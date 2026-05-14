5f2178e1ef6722f4ddb4b5ce8fb9414a51e9ff4c
Muhammad Suleman <suleman0682@$>
2026-05-14 17:52:19 +0500

    Connection limit

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const conn = mysql.createConnection({
   port: process.env.DB_PORT || 12639,
   ssl: {
     rejectUnauthorized: false // Required for Aiven Cloud
-  }
+  },
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 30000 // 30 seconds
 });
 
 conn.connect((err) => {
